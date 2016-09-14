0d957088cd15150f999ad21567e53b8df80ad901
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-09-14 09:38:10 +0200

    Bump version for release

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ This package holds the Optimal Framework source package
 __author__ = 'Nicklas Borjesson'
 
 # Version and release information used by Sphinx for documentation and setuptools for package generation.
-__version__ = '0.11'
-__release__ = '0.11.2'
+__version__ = '0.12'
+__release__ = '0.12.0'
 __copyright__ = '2010-2016, Optimal BPM'
 

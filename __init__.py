1f34824a72747950190d2e7b6a2f81c853f488e9
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-10-14 04:01:31 +0200

    Bump version

--- __init__.py
+++ __init__.py
@@ -5,6 +5,6 @@ __author__ = 'Nicklas Borjesson'
 
 # Version and release information used by Sphinx for documentation and setuptools for package generation.
 __version__ = '0.13'
-__release__ = '0.13.5'
+__release__ = '0.13.7'
 __copyright__ = '2010-2016, Optimal BPM'
 
